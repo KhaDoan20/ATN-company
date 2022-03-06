b1059f08cbfe850974a726f73d7e3efd68756394
KhaDoan209 <doanvinhkha@gmail.com>
2022-03-06 23:36:07 +0700

    third commit

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
-$host_name = "localhost";
-$db_name = "postgres";
-$db_user = "postgres";
-$password="kdoan87720";
+$host_name = "ec2-52-70-186-184.compute-1.amazonaws.com";
+$db_name = "dcj53a69ga9di3";
+$db_user = "xjecpdziyakfam";
+$password="49e772b38d04cb1d465ac90e0d0647b491ae6c798175921f933ab1faf1a87ecc";
 $db_conn_string= "host=$host_name dbname=$db_name user=$db_user password=$password";
 ?>
